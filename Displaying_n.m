bb00dd4f1c1af08f23133cbeb5742ad4880aac77
SaiManojGubbala <saimanojgubbala@gmail.com>
2024-06-09 21:27:59 +0530

    Update Displaying_n.m

--- Displaying_n.m
+++ Displaying_n.m
@@ -30,8 +30,8 @@ title('Decoded Hidden Image');
  mse = double(Red_Part) - Steganographed_Image1;
  mse = double(mse);
   Mse = [];
- for i = 1 : DCT_Block_Size : 1024
-    for j = 1 : DCT_Block_Size : 1024
+ for i = 1 : DCT_Block_Size : n3
+    for j = 1 : DCT_Block_Size : n4
         Block_Matrix = mse(i:i+DCT_Block_Size-1,j:j+DCT_Block_Size-1);
         Mse = [Mse zigzag(Block_Matrix)];
     end
